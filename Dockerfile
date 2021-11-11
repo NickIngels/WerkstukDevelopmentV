077da0706858407833f35ef7037aff5b9f4fd621
Nick Ingels <nick.ingels@student.ehb.be>
2021-11-11 23:49:51 +0100

    feat: created tests, added endpoints & dockerised

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM node
+
+CMD ["npm", "start"]
+COPY package*.json .
+RUN npm install
+EXPOSE 3000
+COPY . .
+WORKDIR /usr/app
---
+++ tests/helper.test.js
@@ -0,0 +1,16 @@
+const index = require("./../index")
+const helpers = require("./../helpers")
+
+// test("testing x", () => {
+//     expect(index.BgRouter(1)).toBeTruthy();
+// })
+
+
+
+const request = require('supertest');
+
+it('tests if connection to endpoint is successful', async () => {
+    const response = await request(index).get('/database/Gebruikers');
+    expect(response.statusCode).toEqual(200);
+    expect(response.body.status).toBe("Gebruikers opgehaald");
+});
