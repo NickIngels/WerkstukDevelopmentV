ac410badda5f3a9e652f1d2097e46a8f7581bb03
Nick Ingels <nick.ingels@student.ehb.be>
2021-12-21 10:14:26 +0100

    last push from werkcollege

--- api/src/helper.test.js
+++ api/src/helper.test.js
@@ -1,35 +1,38 @@
-// const index = require("./index")
-// const helpers = require("./helpers");
-// const request = require('supertest');
-// const {
-//     doesNotMatch
-// } = require("assert");
+const {
+    app
+} = require("./server");
+const helpers = require("./helpers");
+const request = require('supertest');
+const {
+    doesNotMatch
+} = require("assert");
 
 
-// it('tests if connection to endpoint is successful', async () => {
-//     const response = await request(index).get('/database/Gebruikers');
-//     expect(response.statusCode).toEqual(200);
-//     expect(response.body.status).toBe("Gebruikers opgehaald");
-// });
+it('tests if connection to endpoint is successful', async () => {
+    const response = await request(app).get('/database/Gebruikers');
+    expect(response.statusCode).toEqual(200);
+    expect(response.body.status).toBe("Gebruikers opgehaald");
+});
 
-// it('tests if recieving Gebruikerdata is succesful', async () => {
-//     const response = await request(index.app).get('/database/Gebruikers');
-//     expect(response.statusCode).toEqual(200);
-// });
+it('tests if recieving Gebruikerdata is succesful', async () => {
+    const response = await request(app).get('/database/Gebruikers');
+    expect(response.statusCode).toEqual(200);
+});
 
-// it('tests if deleting GebruikerData is succesful', async () => {
-//     const response = await request(index.app).delete('/database/deleteGebruiker/1');
-//     expect(response.statusCode).toEqual(200);
-// });
+it('tests if deleting GebruikerData is succesful', async () => {
+    const response = await request(app).delete('/database/deleteGebruiker/5');
+    expect(response.statusCode).toEqual(200);
+});
 
-// test("testing port length", () => {
-//     expect(helpers.checkPortLength(index.port.toString())).toBeFalsy();
-// })
+//test("testing port length", () => {
+//    expect(helpers.checkPortLength(app.port.toString())).toBeFalsy();
+//})
 
-// test("testing url", () => {
-//     expect(helpers.checkIfURL(`http://localhost:${index.port}`)).toBeFalsy();
-// })
-
-test("testing port length", () => {
-    expect(1 === 1).toBeTruthy();
+//test("testing url", () => {
+//    expect(helpers.checkIfURL(`http://localhost:${app.port}`)).toBeFalsy();
+//})
+describe('helper', () => {
+    test("obviously runs", () => {
+        expect(1 === 1).toBeTruthy();
+    })
 })
--- api/src/index.js
+++ api/src/index.js
@@ -1,103 +1,10 @@
-//Dockerhub access token dc518349-b132-474a-92b1-faf1d44e5520
+const {
+    app
+} = require('./server')
 
-
-// Express
-
-const express = require('express');
-const app = express();
 const port = process.env.APIPORT || 6000;
-const bgRouter = express.Router();
-
-// Knex
-const pg = require('knex')({
-
-    client: 'pg',
-
-    searchPath: ['knex', 'public'],
-
-    connection: process.env.PG_CONNECTION_STRING ? process.env.PG_CONNECTION_STRING : 'postgres://admin:admin@pg:5432/Gebruikers'
-
-});
-
-//This functions creates a user table which contains an id, a name and an email address
-async function createGebruikersTable() {
-    await pg.schema.hasTable('Gebruikers').then(function (exists) {
-        if (!exists) {
-            return pg.schema.createTable('Gebruikers', function (t) {
-                t.increments('UUID').primary();
-                t.string('naam', 100);
-                t.string('email', 100);
-            });
-        }
-    });
-}
-
-//With this function you can post user information
-async function insertGebruikersData() {
-    await pg.table('Gebruikers').insert({
-        naam: "Dirk",
-        email: "Dirkmail"
-    })
-}
-
-//This function gets all user data
-async function gebruikersData() {
-    return await pg.select().table("Gebruikers");
-}
-
-//This function lets you delete a user, when providing an id
-async function deleteGebruiker(UUID) {
-    return await pg.table('Gebruikers').where('UUID', '=', UUID).del()
-}
-
-//This function allows you to update user information using the id of the user
-async function updateGebruiker(UUID) {
-    return await pg.table('Gebruikers').where('UUID', '=', UUID).update('naam', "Nick")
-}
-
-bgRouter.route('/updateGebruiker/:UUID')
-    .patch((req, res) => {
-        updateGebruiker(req.params.UUID);
-        res.send("Updated gebruiker")
-    });
-
-bgRouter.route('/gebruikers')
-    .get((req, res) => {
-        gebruikersData().then((databaseData) => {
-            console.log(databaseData);
-            res.send("Data recieved")
-        })
-    })
-
-bgRouter.route('/deleteGebruiker/:UUID')
-    .delete((req, res) => {
-        deleteGebruiker(req.params.UUID);
-        res.send("Gebruiker deleted")
-    });
-
-// Work in progress
-// app.post('/addGebruiker', (req, res) => {
-//     );
-//     res.send("Gebruiker toegevoegd")
-// });
-
-app.get('/', (req, res) => {
-
-    res.send("Hello world")
-})
-
-app.use('/database', bgRouter);
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 
 });
-
-createGebruikersTable();
-insertGebruikersData();
-
-
-module.exports = {
-    port,
-    app
-}
