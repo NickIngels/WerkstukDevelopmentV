06817d46e872d5fd52487f48a27d61dcf01b2a53
Nick Ingels <nick.ingels@student.ehb.be>
2021-12-22 19:12:12 +0100

    docs: added proper testing description

--- api/src/helper.test.js
+++ api/src/helper.test.js
@@ -14,36 +14,22 @@ it('tests if getting GebruikerData is succesful', async () => {
     expect(response.body.status).toBe("Gebruikers opgehaald");
 });
 
-it('tests if getting GebruikerData is succesful', async () => {
-    const response = await request(app).post('/database/postGebruiker/waytoolongname/mail');
+it('tests if posting GebruikerData with more than 10 characters throws error', async () => {
+    const response = await request(app).post('/database/postGebruiker/waytoolongname/mail/1');
     expect(response.statusCode).toEqual(500);
 });
 
-it('tests if getting GebruikerData is succesful', async () => {
-    const response = await request(app).post('/database/postGebruiker/goodname/mail');
+it('tests if posting GebruikerData is succesful', async () => {
+    const response = await request(app).post('/database/postGebruiker/goodname/mail/1');
     expect(response.statusCode).toEqual(200);
 });
 
-// it('tests if recieving Gebruikerdata is succesful', async () => {
-//     const response = await request(app).get('/database/Gebruikers');
-//     expect(response.statusCode).toEqual(200);
-// });
+it('tests if posting categorieData is succesful', async () => {
+    const response = await request(app).post('/database/postCategorie/testing/7');
+    expect(response.statusCode).toEqual(200);
+});
 
 it('tests if deleting GebruikerData is succesful', async () => {
     const response = await request(app).delete('/database/deleteGebruiker/5');
     expect(response.statusCode).toEqual(200);
 });
-
-//test("testing port length", () => {
-//    expect(helpers.checkPortLength(app.port.toString())).toBeFalsy();
-//})
-
-//test("testing url", () => {
-//    expect(helpers.checkIfURL(`http://localhost:${app.port}`)).toBeFalsy();
-//})
-
-describe('helper', () => {
-    test("obviously runs", () => {
-        expect(1 === 1).toBeTruthy();
-    })
-})
